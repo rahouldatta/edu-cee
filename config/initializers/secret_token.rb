94a3e7dc59f749b53da83cd1673801f8028314c1
Rahoul Datta <rahouldatta@gmail.com>
2013-07-30 22:48:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduCee::Application.config.secret_token = '8f03fcb637cd1d5023acde1f617e72dca7da8502d48a46ffa12ec729a466894b5518206c958d3db8280cf635fcfca7d829c47526cf98b57df21e7c97e79d3d90'
